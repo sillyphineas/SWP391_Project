a909fa50dd5ee7faeee9cf9958bfc7606adaade4
ASUS <ASUS@NamNguyen-K18FPT>
2025-01-18 22:35:21 +0700

    Update 18/1/2025 - Demo Controller Branch (NamNH)

---
+++ src/java/Controller/BlogControler.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+/**
+ *
+ * @author ASUS
+ */
+public class BlogControler {
+    public static void main(String[] args) {
+        String var = "nam";
+        System.out.println(var);
+    }
+}
