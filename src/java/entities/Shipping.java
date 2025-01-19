90969d351070c23e120835b14b4315bddb876853
quangdat25 <dquangdat04@gmail.com>
2025-01-19 18:05:53 +0700

    merge

--- src/java/entities/Shipping.java
+++ src/java/entities/Shipping.java
@@ -4,10 +4,94 @@
  */
 package entities;
 
+import java.util.Date;
+
 /**
  *
  * @author HP
  */
 public class Shipping {
-    
+    private int ShippingID;
+    private int OrderID;
+    private int ShipperID;
+    private String ShippingStatus;
+    private Date EstimatedArrival;     
+    private Date ActualArrival;      
+    private Date ShippingDate;       
+
+    public Shipping() {
+    }
+
+    public Shipping(int ShippingID, int OrderID, int ShipperID, String ShippingStatus, Date EstimatedArrival, Date ActualArrival, Date ShippingDate) {
+        this.ShippingID = ShippingID;
+        this.OrderID = OrderID;
+        this.ShipperID = ShipperID;
+        this.ShippingStatus = ShippingStatus;
+        this.EstimatedArrival = EstimatedArrival;
+        this.ActualArrival = ActualArrival;
+        this.ShippingDate = ShippingDate;
+    }
+
+    public int getShippingID() {
+        return ShippingID;
+    }
+
+    public void setShippingID(int ShippingID) {
+        this.ShippingID = ShippingID;
+    }
+
+    public int getOrderID() {
+        return OrderID;
+    }
+
+    public void setOrderID(int OrderID) {
+        this.OrderID = OrderID;
+    }
+
+    public int getShipperID() {
+        return ShipperID;
+    }
+
+    public void setShipperID(int ShipperID) {
+        this.ShipperID = ShipperID;
+    }
+
+    public String getShippingStatus() {
+        return ShippingStatus;
+    }
+
+    public void setShippingStatus(String ShippingStatus) {
+        this.ShippingStatus = ShippingStatus;
+    }
+
+    public Date getEstimatedArrival() {
+        return EstimatedArrival;
+    }
+
+    public void setEstimatedArrival(Date EstimatedArrival) {
+        this.EstimatedArrival = EstimatedArrival;
+    }
+
+    public Date getActualArrival() {
+        return ActualArrival;
+    }
+
+    public void setActualArrival(Date ActualArrival) {
+        this.ActualArrival = ActualArrival;
+    }
+
+    public Date getShippingDate() {
+        return ShippingDate;
+    }
+
+    public void setShippingDate(Date ShippingDate) {
+        this.ShippingDate = ShippingDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Shipping{" + "ShippingID=" + ShippingID + ", OrderID=" + OrderID + ", ShipperID=" + ShipperID + ", ShippingStatus=" + ShippingStatus + ", EstimatedArrival=" + EstimatedArrival + ", ActualArrival=" + ActualArrival + ", ShippingDate=" + ShippingDate + '}';
+    }
+            
+            
 }
